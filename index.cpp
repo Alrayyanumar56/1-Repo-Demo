d9f600ea8cdd5e18b6a5daad037db7cd5777d5f2
Alrayyanumar56 <aryanumar56@gmail.com>
2025-03-30 14:54:21 +0500

    add comments

--- index.cpp
+++ index.cpp
@@ -1,16 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int area(int,int);
-int main()
-{
-	int length,width;
-	cout<<"Enter the Length & Width of rectangle: "<<endl;
-	cin>>length>>width;
-	cout<<"Area of Rectangle: "<<area(length,width);
+
+int area(int, int); // Function prototype
+
+int main() {
+    int length, width;
+    
+    cout << "Enter the Length & Width of the rectangle: ";
+    cin >> length >> width;
+    
+    cout << "Area of Rectangle: " << area(length, width) << endl;
+    
+    return 0; // Indicating successful execution
 }
 
-int area(int a,int b)
-{
-	int c=a*b;
-	return c;
+int area(int a, int b) {
+    return a * b; // Directly returning the product
 }
